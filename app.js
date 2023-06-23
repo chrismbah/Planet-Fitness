51c2a29b21fd7883d7dfe348a1b591dfc9a4e5d2
Chris Mbah <mbahchris46@gmail.com>
2023-06-23 22:48:04 +0100

    Menu toggle functionality and responsiveness

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const nav=document.querySelector("nav");
 window.addEventListener("scroll",function(){
     nav.classList.toggle("scrolled",window.scrollY>10)
 })
+
+let menu=document.querySelector("#menu-btn");
+let navlist=document.querySelector(".navlist");
+
+menu.onclick=function(){
+ menu.classList.toggle("bx-x");
+ navlist.classList.toggle("hidden")
+}
